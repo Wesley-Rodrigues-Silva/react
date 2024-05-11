5d0bb3ed7b44d9f85bfdc09c4825b6dce2d275c3
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-05-10 22:06:51 -0300

    att

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
+import List from './components/List';
 function App() {
   const nome = 'Maria'
   return (   
     <div className="App">
+      <h1>Tetando CSS</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Wesley"/>
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
@@ -13,8 +18,9 @@ function App() {
       nome="Wesley" 
       idade="26" 
       profissao="Programador" 
-      foto="https://via.placeholder.com/150"
+      foto="https://via.placeholder.com/150"      
     />
+    <List/>
     </div>
   );
 }
